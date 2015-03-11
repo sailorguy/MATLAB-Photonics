fe6ac10879b47f21d626ab428bef7acf2c2a0d3b
sailorguy <jacobkeilman@gmail.com>
2015-03-11 12:00:14 +0100

    Moved java tree node class

---
+++ Java/TreeNodeUserClass/src/java_treeNodeUserClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jkeilman3
+ */
+public class java_treeNodeUserClass {
+    
+    public String nodeName;
+    public int nodeIndex;
+    
+    
+    //Empty constructor
+    public java_treeNodeUserClass(){
+        
+        
+    }
+    
+    
+    @Override
+    public String toString(){
+        return this.nodeName;
+    }
+    
+    
+    
+}
